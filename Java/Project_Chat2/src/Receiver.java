1f68858ee9ecafc839d13cb42e96dcdd172dc60e
오 연 주 <only6hwa@gmail.com>
2018-07-26 18:29:31 +0900

    07월 26일(1차 프로젝트 2일째_3)

---
+++ Java/Project_Chat2/src/Receiver.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.net.Socket;
+
+public class Receiver extends Thread {
+
+	Socket socket;
+	BufferedReader in = null;
+	
+	public Receiver(Socket socket) {
+		
+		this.socket = socket;
+		
+		try {
+			in = new BufferedReader(new InputStreamReader (
+										this.socket.getInputStream() ));
+		} catch (Exception e) {
+			System.out.println( "예외 : " + e );
+		}
+	}
+	
+	@Override
+	public void run() {
+		while (in!=null) {
+			try {
+				System.out.println(">>" + in.readLine());
+			} catch (java.net.SocketException ne) {
+				break;
+				
+			} catch(Exception e) {
+				System.out.println("예외 : " + e );
+			} 
+			
+		}
+		
+		try {
+			in.close();
+		} catch (Exception e) {
+			System.out.println("예외 3 : " + e );
+		}
+
+	}
+
+}
