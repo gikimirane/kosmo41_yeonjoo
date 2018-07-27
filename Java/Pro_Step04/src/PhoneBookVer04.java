1d118de6f51cdcb65a6d6e2aac6f042ce865932f
오 연 주 <only6hwa@gmail.com>
2018-07-27 19:19:03 +0900

    07월 27일 금요일수업(1차 프로젝트 3일차_추가)

--- Java/Pro_Step04/src/PhoneBookVer04.java
+++ Java/Pro_Step04/src/PhoneBookVer04.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /* 04 단계 : 전화번호 관리 프로그램 
-             ㄴ birth와 관련된 코드 - 삭제 
+             ㄴ birth와 관련된 코드 - 삭제 [ v ]
              ㄴ 대학동기 전화번호 및 회사동료 전화번호 저장 - 추가 */
 
 //데이터 저장 클래스
@@ -13,7 +13,7 @@ class PhoneInfo {
 	public PhoneInfo(String name, String num) {
 
 		this.name = name;
-		phoneNum = num; 
+		phoneNum = num;
 
 	}
 
@@ -22,160 +22,193 @@ class PhoneInfo {
 		System.out.println("name : " + name);
 		System.out.println("phone : " + phoneNum);
 
-		System.out.println("");
 	}
 
 }
-	
 
-class PhoneUnivInfo extends PhoneInfo {  // name,num 상속
+class PhoneUnivInfo extends PhoneInfo { // name,num 상속
 
 	String major, year;
 
-	public PhoneUnivInfo(String name,String num,String major, String year) {
+	public PhoneUnivInfo(String name, String num, String major, String year) {
 
-		super(name,num);      // 자신을 가리키는 키워드가 this 라면 부모를 가리키는 키워드는 super
- 		this.major = major;
+		super(name, num);      // 자신을 가리키는 키워드가 this 라면 부모를 가리키는 키워드는 super
+		this.major = major;
 		this.year = year;
 
 	}
 
-	
-class PhoneCompanyInfo extends PhoneInfo {  
+	public void showPhoneInfo() {
+
+		super.showPhoneInfo();
+		System.out.println("major : " + major);
+		System.out.println("year  : " + year);
+
+	}
+
+	class PhoneCompanyInfo extends PhoneInfo {
 
 		String company;
 
-		public PhoneCompanyInfo(String name,String num,String company) {
+		public PhoneCompanyInfo(String name, String num, String company) {
 
-			super(name,num);
+			super(name, num);
 			this.company = company;
 
-		}	
-		
-	
-//흐름 구성 클래스
-
-class PhoneBookManager {
-
-	final int MAX = 100;                      //final 메소드가 더이상 오버라이딩 되지 않아야 할 때 사용 
-	PhoneInfo[] infoSt = new PhoneInfo[MAX];
-
-	int Count = 0;
-
-	public void inputData() {
-
-		System.out.println("<< 일반 데이터 입력 >>");
-
-		System.out.print("이    름 : ");
-		String name = MenuV.keyboard.nextLine();
-		System.out.print("전화번호 : ");
-		String phone = MenuV.keyboard.nextLine();
-
-		infoSt[Count++] = new PhoneInfo(name, phone);
-		System.out.println("<< 일반 데이터 입력 완료 >>");
-	}
-
-	public void U_inputData() {
-
-		System.out.println("<< 대학동기 데이터 입력 >>");
-
-		System.out.print("이    름 : ");
-		String name = MenuV.keyboard.nextLine();
-		System.out.print("전화번호 : ");
-		String phone = MenuV.keyboard.nextLine();
-		System.out.print("전    공 : ");
-		String major = MenuV.keyboard.nextLine();
-		System.out.print("학    년 : ");
-		String year = MenuV.keyboard.nextLine();
-
-		//infoSt[Count++] = new PhoneInfo(name, phone, major, year);
-		
-		System.out.println("<< 데이터 입력 완료 >>");
-	}
-	
-	public void C_inputData() {
-
-		System.out.println("<< 데이터 입력 >>");
-
-		System.out.print("이름 : ");
-		String name = MenuV.keyboard.nextLine();
-		System.out.print("전화번호 : ");
-		String phone = MenuV.keyboard.nextLine();
-
-		infoSt[Count++] = new PhoneInfo(name, phone);
-		System.out.println("<< 데이터 입력 완료 >>");
-	}
-	public void searchData() {
-
-		System.out.println("<< 데이터 검색 >> ");
-
-		System.out.print(" 이름으로 검색 : ");
-		String name = MenuV.keyboard.nextLine();
-
-		int dataIdx = search(name);
-		if (dataIdx < 0) {
-			System.out.println("<< 해당하는 데이터없음 >> \n");
-		} else {
-			infoSt[dataIdx].showPhoneInfo();
-			System.out.println(" << 데이터 검색 완료 >> \n");
 		}
-	}
 
-	public void delData() {
-		
-		System.out.println("<< 데이터 삭제 >>");
+		public void showPhoneInfo() {
 
-		System.out.print("이름으로 검색 후 삭제 : ");
-		String name = MenuV.keyboard.nextLine();
-
-		int dataIdx = search(name);
-
-		if (dataIdx < 0) {
-			System.out.println("<< 해당하는 데이터없음 >> \n");
-		} else {
-
-			for (int idx = dataIdx; idx < (Count - 1); idx++)
-				infoSt[idx] = infoSt[idx + 1];
-
-			Count--;
-			System.out.println("<< 데이터 검색 완료 >> \n");
+			super.showPhoneInfo();
+			System.out.println("company : " + company);
 		}
-	}
 
-	private int search(String name) {
+		// 흐름 구성 클래스
 
-		for (int idx = 0; idx < Count; idx++) {
-			PhoneInfo curInfo = infoSt[idx];
-			if (name.compareTo(curInfo.name) == 0)
-				return idx;
+		class PhoneBookManager {
+
+			final int MAX = 100; // final 메소드가 더이상 오버라이딩 되지 않아야 할 때 사용
+			PhoneInfo[] infoSt = new PhoneInfo[MAX];
+
+			int Count = 0;
+
+			private PhoneInfo i_Info() {
+
+				System.out.println("<< 일반 데이터 입력 >>");
+
+				System.out.print("이    름 : ");
+				String name = MenuV.keyboard.nextLine();
+				System.out.print("전화번호 : ");
+				String phone = MenuV.keyboard.nextLine();
+
+				return new PhoneInfo(name, phone);
+
+			}
+
+			private PhoneInfo u_Info() {
+
+				System.out.println("<< 대학동기 데이터 입력 >>");
+
+				System.out.print("이    름 : ");
+				String name = MenuV.keyboard.nextLine();
+				System.out.print("전화번호 : ");
+				String phone = MenuV.keyboard.nextLine();
+				System.out.print("전    공 : ");
+				String major = MenuV.keyboard.nextLine();
+				System.out.print("학    년 : ");
+				String year = MenuV.keyboard.nextLine();
+
+				return new PhoneUnivInfo(name, phone, major, year);
+
+			}
+
+			private PhoneInfo c_Info() {
+
+				System.out.println("<< 회사동료 데이터 입력 >>");
+
+				System.out.print("이    름 : ");
+				String name = MenuV.keyboard.nextLine();
+				System.out.print("전화번호 : ");
+				String phone = MenuV.keyboard.nextLine();
+				System.out.print("회 사 명 : ");
+				String company = MenuV.keyboard.nextLine();
+
+				return new PhoneCompanyInfo(name, phone, company);
+			}
+			
+			
+			public void inputdata() {
+				System.out.println("<< 데이터 입력 >>");
+				System.out.println("1.일반 2.대학 3.회사 ");
+				System.out.println("선택 값 입력 : ");
+				
+				int choice = MenuV.keyboard.nextInt();
+				MenuV.keyboard.nextLine();
+				PhoneInfo info = null;
+				
+				switch (choice) {
+				case 1:
+					info = i_Info();
+					break;
+				case 2:
+					info = u_Info();
+					break;
+				case 3:
+					info = c_Info();
+					break;
+				}
+				
+			}
+
+			public void searchData() {
+
+				System.out.println("<< 데이터 검색 >> ");
+
+				System.out.print(" 이름으로 검색 : ");
+				String name = MenuV.keyboard.nextLine();
+
+				int dataIdx = search(name);
+				if (dataIdx < 0) {
+					System.out.println("<< 해당하는 데이터없음 >> \n");
+				} else {
+					infoSt[dataIdx].showPhoneInfo();
+					System.out.println(" << 데이터 검색 완료 >> \n");
+				}
+			}
+
+			public void delData() {
+
+				System.out.println("<< 데이터 삭제 >>");
+
+				System.out.print("이름으로 검색 후 삭제 : ");
+				String name = MenuV.keyboard.nextLine();
+
+				int dataIdx = search(name);
+
+				if (dataIdx < 0) {
+					System.out.println("<< 해당하는 데이터없음 >> \n");
+				} else {
+
+					for (int idx = dataIdx; idx < (Count - 1); idx++)
+						infoSt[idx] = infoSt[idx + 1];
+
+					Count--;
+					System.out.println("<< 데이터 검색 완료 >> \n");
+				}
+			}
+
+			private int search(String name) {
+
+				for (int idx = 0; idx < Count; idx++) {
+					PhoneInfo curInfo = infoSt[idx];
+					if (name.compareTo(curInfo.name) == 0)
+						return idx;
+				}
+				return -1;
+			}
+
 		}
-		return -1;
-	}
 
-	
-}	
-	
-class MenuV {
+		class MenuV {
 
-		public static Scanner keyboard = new Scanner(System.in);  
+			public static Scanner keyboard = new Scanner(System.in);
 
-		public static void showMenu() {
+			public static void showMenu() {
 
-			System.out.println("");
-			System.out.println("원하시는 메뉴를 선택 하세요");
-			System.out.println("데 이 터 입력  : 1 ");
-			System.out.println("데 이 터 검색  : 2 ");
-			System.out.println("데 이 터 삭제  : 3 ");
-			System.out.println("프로그램 종료  : 4 ");
-			System.out.println("");
-			System.out.println("선택 값 입력 :");
+				System.out.println("");
+				System.out.println("원하시는 메뉴를 선택 하세요");
+				System.out.println("데 이 터 입력  : 1 ");
+				System.out.println("데 이 터 검색  : 2 ");
+				System.out.println("데 이 터 삭제  : 3 ");
+				System.out.println("프로그램 종료  : 4 ");
+				System.out.println("");
+				System.out.println("선택 값 입력 :");
 
+			}
 		}
-	}
 
-	
-	// 실행(메인) 클래스
-	
+		// 실행(메인) 클래스
+
 class PhoneBookVer03 {
 
 		public static void main(String[] args) {
@@ -209,4 +242,3 @@ class PhoneBookVer03 {
 		}
 
 	}
-
