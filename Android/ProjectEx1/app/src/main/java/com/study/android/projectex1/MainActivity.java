ee07fc55cc038b8f6eb4a809990aa6561b2e4aa2
only6hwa <only6hwa@gmail.com>
2018-10-04 21:53:54 +0900

    10월 4일 목요일수업(프로젝트 2일차)

--- Android/ProjectEx1/app/src/main/java/com/study/android/projectex1/MainActivity.java
+++ Android/ProjectEx1/app/src/main/java/com/study/android/projectex1/MainActivity.java
@@ -1,58 +1,175 @@
 package com.study.android.projectex1;
 
 import android.app.AlarmManager;
+import android.app.DatePickerDialog;
 import android.app.PendingIntent;
-import android.content.Context;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "lecture";
 
+    //시간 설정을 위한 객체
+    Calendar Time;
 
-    AlarmManager am;
-    Intent intent;
-    PendingIntent receiver;
+    //알람 설정을 위한 객체
+    private Intent intent;
+    private AlarmManager alarmManager;
+    private PendingIntent ServicePending;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy년 MM월 dd일 mm분 ss초");
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    TextView textView;
 
-        am = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
+    DatePickerDialog.OnDateSetListener
+            eDateSetListener = new DatePickerDialog.OnDateSetListener() {
 
-    intent = new Intent(this, AlarmReceiver.class);
-    receiver =PendingIntent.getBroadcast(this,
-            0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-}
+        @Override
+        public void onDateSet
+                (DatePicker view, int year, int monthOfYear, int dayOfMonth) {
 
-    public void onBtn1Clicked(View v) {
-        //알람시간. 60초 후
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTimeInMillis(System.currentTimeMillis());
-        calendar.add(Calendar.SECOND, 10);
+            Time.set(Calendar.YEAR,year);
+            Time.set(Calendar.MONTH,monthOfYear);
+            Time.set(Calendar.DAY_OF_MONTH,dayOfMonth);
 
-        //알람등록
-        am.set(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),receiver);
+            updateLabel();
+
+        }
+    };
+
+        private TimePickerDialog.OnTimeSetListener sTimeSetListener =
+                new TimePickerDialog.OnTimeSetListener() {
+
+                    @Override
+                    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+
+                        Time.set(Calendar.HOUR_OF_DAY,hourOfDay);
+                        Log.d("check","1 = " + hourOfDay);
+                        Time.set(Calendar.MINUTE,minute);
+                        Log.d("check","2 = " + minute);
+                        Time.set(Calendar.SECOND, 0);
+
+                        updateLabel();
+                    }
+                };
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            //타임 피커, 데이트 피커 리스너 및 아이디 등록
+            Time = Calendar.getInstance();
+
+            //알람 설정, 해제 버튼
+            Button.OnClickListener bClickListener = new View.OnClickListener() {
+
+                public void onClick(View v) {
+                    switch (v.getId()) {
+                        case R.id.setAlarm:
+
+                            setAlarm();
+                            break;
+
+                        case R.id.removeAlarm:
+
+                            removeAlarm();
+                            break;
+
+                        case R.id.button:
+                            new TimePickerDialog(MainActivity.this, sTimeSetListener,
+                                    Time.get(Calendar.HOUR_OF_DAY), Time.get(Calendar.MINUTE), false).show();
+
+                            new DatePickerDialog(MainActivity.this, eDateSetListener,
+                                    Time.get(Calendar.YEAR), Time.get(Calendar.MONTH), Time.get(Calendar.DAY_OF_MONTH)).show();
+                            break;
+
+                        case R.id.repeatAlarm:
+
+                            setRepeatAlarm();
+                            break;
+
+                    }
+                }
+            };
+
+            findViewById(R.id.setAlarm).setOnClickListener(bClickListener);
+            findViewById(R.id.removeAlarm).setOnClickListener(bClickListener);
+            findViewById(R.id.button).setOnClickListener(bClickListener);
+            findViewById(R.id.repeatAlarm).setOnClickListener(bClickListener);
+
+            alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+            textView = (TextView) findViewById(R.id.textView);
+
+            updateLabel();
+        }
+
+        private void updateLabel() {
+            textView.setText(simpleDateFormat.format(Time.getTime()));
+        }
+
+        void setAlarm() {
+            //Receiver로 보내기 위한 인텐트
+            //intent = new Intent(this, AlarmReceiver.class);
+            intent = new Intent("AlarmReceiver");
+            //PendingIntent.getBroadcast(Context context, int requestCod, Intent intent int flag);
+
+            ServicePending = PendingIntent.getBroadcast(
+                    MainActivity.this, 111, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+            //정해진 시간에 알람 설정
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, Time.getTimeInMillis(), 1000, ServicePending);
+            //alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, Time.getTimeInMillis(), 1000, ServicePending);
+
+            //Millis * Second * Minute
+
+            Toast.makeText(getBaseContext(), "알람 설정" + Time.getTime(), Toast.LENGTH_SHORT).show();
+
+        }
+
+        void removeAlarm() {
+
+            intent = new Intent("AlarmReceiver");
+            //PendingIntent.getBroadcast(Context context, int requestCod, Intent intent, int flag);
+
+            ServicePending = PendingIntent.getBroadcast(
+                    MainActivity.this, 111, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            Toast.makeText(getBaseContext(), "알람 해제" + Time.getTime(), Toast.LENGTH_SHORT).show();
+
+            alarmManager.cancel(ServicePending);
+        }
+
+        void setRepeatAlarm() {
+            //Receiver로 보내기 위한 인텐트
+            //intent = new Intent(this, AlarmReceiver.class);
+            intent = new Intent("AlarmReceiver");
+            //PendingIntent.getBroadcast(Context context, int requestCod, Intent intent int flag);
+
+            ServicePending = PendingIntent.getBroadcast(
+                    MainActivity.this, 111, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+            Log.d("ServicePending : ", "" + ServicePending.toString());
+
+            //정해진 시간에 알림 설정
+            // alarmManager.set(AlarmManager.RTC_WAKEUP,Time.getTimeInMillis(),servicePending);
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, Time.getTimeInMillis(), 20000, ServicePending);
+            //Millis * Second * Minute
+
+            Toast.makeText(getBaseContext(), "알람 설정" + Time.getTime(), Toast.LENGTH_SHORT).show();
+
+        }
     }
 
-    public void onBtn2Clicked(View v) {
-        //10초당 한번 알람 등록 : 24 * 60 * 60 * 1000
-        am.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                SystemClock.elapsedRealtime(),
-                10 * 1000, receiver);
-    }
 
-    public void onBtn3Clicked(View v) {
-        am.cancel(receiver);
-    }
-
-}
-
-}
